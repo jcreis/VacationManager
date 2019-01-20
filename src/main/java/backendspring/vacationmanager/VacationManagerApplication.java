e233eccdef37c9a155d6a4f188fe84da697a73b9
João Reis <jc.reis@campus.fct.unl.pt>
2019-01-20 23:06:22 +0000

    First Commit

---
+++ src/main/java/backendspring/vacationmanager/VacationManagerApplication.java
@@ -0,0 +1,14 @@
+package backendspring.vacationmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VacationManagerApplication.class, args);
+    }
+
+}
+
